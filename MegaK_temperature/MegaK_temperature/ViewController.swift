2e38ab199ed4c877203448d0fb2808bad5345b25
AlexLexa32 <105227962+AlexLexa32@users.noreply.github.com>
2022-09-04 21:06:24 +0300

    Add files via upload

---
+++ MegaK_temperature/MegaK_temperature/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MegaK_temperature
+//
+//  Created by Алексей Плотников on 04.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
