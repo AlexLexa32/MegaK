d49e4e7c114c1280e079f1467909ee25ce5b6da8
AlexLexa32 <105227962+AlexLexa32@users.noreply.github.com>
2022-09-04 13:29:37 +0300

    Add files via upload

---
+++ MegaK_lite/Main (1).java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.lang.String;
+public class Main {
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+	    Scanner scanChar = new Scanner(System.in);
+		int x, y;
+		String q;
+		x = scan.nextInt();
+		q = scanChar.nextLine();
+	    y = scan.nextInt();
+        switch (q) {
+	        case "+":
+	            System.out.println(x+y);
+	            break;
+	        case "-":
+	            System.out.println(x-y);
+	            break;
+	        case "*":
+	            System.out.println(x*y);
+	            break;
+	        case "/":
+	            System.out.println(x/y);
+	            break;
+	        default:
+	            System.out.println(x%y);
+	    }
+	}
+}
