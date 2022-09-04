3a8a7973f2452f6a2127290617d56169b0630b80
AlexLexa32 <105227962+AlexLexa32@users.noreply.github.com>
2022-09-04 12:52:30 +0300

    Update MegaK_lite.cpp

--- MegaK_lite/MegaK_lite.cpp
+++ MegaK_lite/MegaK_lite.cpp
@@ -1,41 +1,23 @@
 #include <iostream>
-#include <string>
 using namespace std;
-int main()
-{
+int main() {
     int q, w;
-    q = 0;
-    w = 0;
-    
     char s;
-    
-   while(true){
-       cin>>q>>s>>w;
-       
+    cin >> q >> s >> w;
     switch(s){
-    case '*':
-       cout<<w*q;
-       break;
-        
-    case '/':
-       cout<<q/w;
-       break;
-        
-    case '-':
-       cout<<q-w;
-       break;
-        
-    case '+':
-       cout<<w+q;
-       break;
-       
-       case '!':
-       return 0;
-       
-       default:
-        return 0;
+        case '*':
+           cout << w*q;
+           break;
+        case '/':
+            cout << q/w;
+           break;
+        case '-':
+            cout << q-w;
+            break;
+        case '+':
+            cout << w+q;
+            break;
+        default:
+            cout << w%q;
     }
-    cin.get();
-    
-}
 }
